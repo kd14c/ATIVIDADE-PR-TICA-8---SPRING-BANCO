9744588a8ae5086040ce0a196acfbc065aa3a4ee
kd14c <carlos.campolongo@fatec.sp.gov.br>
2025-11-03 23:07:02 -0300

    Create EnderecoRepository.java

---
+++ br/edu/fatec/cepbanco/repository/EnderecoRepository.java
@@ -0,0 +1,12 @@
+package br.edu.fatec.cepbanco.repository;
+
+import br.edu.fatec.cepbanco.model.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+    Optional<Endereco> findByCep(String cep);
+}
